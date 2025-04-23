43ed25c6916dd7f0561985bc24c44944b58faae7
krmistry <kelly.r.mistry@gmail.com>
2025-04-23 10:16:09 -0700

    Add single threshold function to this script

--- Scripts/IBM_threshold_funs.R
+++ Scripts/IBM_threshold_funs.R
@@ -1,5 +1,33 @@
 ## Threshold function to use with IBM data to determine what the true conditions are
 
+## Function setting threshold to turn ADS on and off (if x-large snakes reach 0, turn off, 
+## otherwise keep on), to be used to evaluate next methods at each estimation step
+strat_threshold_fun <- function(mean_N_df,
+                                  xlarge_density = 0.1,
+                                  area = area_size,
+                                  size_class = size_class_names) {
+  # Condition (default is initial)
+  condition <- "initial"
+  # Adding a column with density
+  mean_N_df$mean_density <- mean_N_df$N/area
+  # Identifying final time step 
+  last_quarter <- tail(sort(unique(mean_N_df$Quarter)),1)
+  # Separating final time step
+  last_quarter_means <- mean_N_df[mean_N_df$Quarter == last_quarter, ]
+  
+  ## Calculating mean densities 
+  # X-large size class
+  xlarge_mean_density <- last_quarter_means$N[last_quarter_means$size_class == size_class[4]]/area
+  
+  
+  ## Threshold 1: Estimated mean density is of x-large size class is <=0.01 snake/ha 
+  if(xlarge_mean_density <= xlarge_density) {
+    condition <- "threshold_1"
+  } 
+  
+  return(condition)
+}
+
 
 ## Function to evaluate IBM results given threshold of 1/10 snakes per ha over 1,150 mm SVL (x-large size )
 IBM_threshold_fun <- function(data,
@@ -18,8 +46,8 @@ IBM_threshold_fun <- function(data,
     possible_sets <- seq(4, 40, 4)
     final_quarter <- possible_sets[which(possible_sets > final_quarter)][1]
   }
-  # Determine which condition should have been triggered after each estimation step (every 2 quarters)
-  for(quarter in seq(4, final_quarter-4, 4)) {
+  # Determine which condition should have been triggered after each estimation step 
+  for(quarter in possible_sets[possible_sets <= final_quarter]) {
     # Separating final time step
     last_quarter_data <- data[which(data$Quarter == quarter & data$size_class != "total"), ]
     ## Calculating X-large size class density
